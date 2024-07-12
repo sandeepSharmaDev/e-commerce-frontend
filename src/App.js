b0a683156b3206041061c09119fb68a2a89fc908
sandeep-aham <sandeep.devapp@gmail.com>
2024-07-12 17:49:50 +0530

    project setup done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import {Container} from 'react-bootstrap';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   <>
+   <div>
+   <Navbar />
+
+   <Container>
+     <h1>Welcome to React App</h1>
+   </Container>
+
+   <Footer />
+
+   </div>
+
+
+  
+   
+   </>
+  )
 }
 
-export default App;
+export default App
